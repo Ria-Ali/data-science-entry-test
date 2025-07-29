1e4cf6e35400eabd4b7a2fbdc51f9e867a468474
Ria-Ali <222045146+Ria-Ali@users.noreply.github.com>
2025-07-30 03:38:39 +0800

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,1 +1,15 @@
+def update_dictionary(dct, key, value):
+    if not isinstance(dct, dict):
+        return -1  
 
+    if key in dct:
+        print("Original value for key '{}' was: {}".format(key, dct[key]))
+
+    dct[key] = value  
+    return dct
+
+result1 = update_dictionary({}, "name", "Alice")
+print(result1)
+
+result2 = update_dictionary({"age": 25}, "age", 26)
+print(result2)
