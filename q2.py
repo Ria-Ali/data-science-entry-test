db2a887ae0bc779b58e300cbad1f0af5b586ad57
Ria-Ali <222045146+Ria-Ali@users.noreply.github.com>
2025-07-30 03:06:56 +0800

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,2 +1,15 @@
- if not isinstance(lst, list):
+def find_and_replace(lst, find_val, replace_val):
+    if not isinstance(lst, list):
         return -1
+        
+    for i in range(len(lst)):
+        if lst[i] == find_val:
+            lst[i] = replace_val
+
+    return lst
+
+result1 = find_and_replace([1, 2, 3, 4, 2, 2], 2, 5)
+print(result1)
+
+result2 = find_and_replace(["apple", "banana", "apple"], "apple", "orange")
+print(result2)
