f80dd9c1c4caabf55098705b4db175068dc189e8
Ria-Ali <222045146+Ria-Ali@users.noreply.github.com>
2025-07-31 07:32:02 +0800

    Update q6.py

--- q6.py
+++ q6.py
@@ -1,10 +1,10 @@
-def find_first_negative(l\lst):
-    i = 0 
+def find_first_negative(lst):
+    i = 0
     while i < len(lst):
         if lst[i] < 0:
-            return lst[i]  
-        i += 1 
-    return 
+            return lst[i]
+        i += 1
+    return "No negatives"
 
 print(find_first_negative([3, 5, -1, 7, -2, 8]))
 print(find_first_negative([2, 10, 7, 0]))
